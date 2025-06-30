9dd97f678c462f8acec2756a67fcf15e47bffb09
Philipp Siedler <p.d.siedler@gmail.com>
2025-06-30 15:28:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,47 @@
 import streamlit as st
-import random
 from collections import Counter
 
-all_images = [
-    "https://placekitten.com/200/300",
-    "https://placebear.com/200/300",
-    "https://placekitten.com/210/300",
-    "https://placebear.com/210/300"
+# define explicit pairs
+image_pairs = [
+    ("https://placekitten.com/200/300", "https://placebear.com/200/300"),
+    ("https://placekitten.com/210/300", "https://placebear.com/210/300"),
+    ("https://placekitten.com/220/300", "https://placebear.com/220/300")
 ]
 
 if "pair_counter" not in st.session_state:
     st.session_state.pair_counter = 0
-if "shown_pairs" not in st.session_state:
-    st.session_state.shown_pairs = []
 if "choices" not in st.session_state:
     st.session_state.choices = []
 
 st.title("Image Pair Chooser")
 
-st.progress(st.session_state.pair_counter / 10.0)
+st.progress(st.session_state.pair_counter / len(image_pairs))
 
 if st.button("Start Over"):
     st.session_state.pair_counter = 0
-    st.session_state.shown_pairs = []
     st.session_state.choices = []
     st.experimental_rerun()
 
-def advance_state():
-    st.session_state.pair_counter += 1
-
-if st.session_state.pair_counter < 10:
-    while True:
-        img1, img2 = random.sample(all_images, 2)
-        if (img1, img2) not in st.session_state.shown_pairs and (img2, img1) not in st.session_state.shown_pairs:
-            st.session_state.shown_pairs.append((img1, img2))
-            break
+if st.session_state.pair_counter < len(image_pairs):
+    img1, img2 = image_pairs[st.session_state.pair_counter]
 
     col1, col2 = st.columns(2)
     with col1:
         st.image(img1, use_container_width=True)
         if st.button("Select left image"):
             st.session_state.choices.append(img1)
-            advance_state()
+            st.session_state.pair_counter += 1
 
     with col2:
         st.image(img2, use_container_width=True)
         if st.button("Select right image"):
             st.session_state.choices.append(img2)
-            advance_state()
+            st.session_state.pair_counter += 1
 else:
-    st.write("## Results after 10 pairs")
+    st.write("## Results after viewing pairs")
     scores = Counter(st.session_state.choices)
     st.bar_chart(scores)
     if st.button("Restart"):
         st.session_state.pair_counter = 0
-        st.session_state.shown_pairs = []
         st.session_state.choices = []
         st.experimental_rerun()
